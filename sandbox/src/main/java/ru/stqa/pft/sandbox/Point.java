d16273d6359ca5f32aa3356d27face32509b0d3d
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-02-02 15:18:45 +0300

    В классе Point оставлен только метод distanceToPointMethod(Point p). Тесты переписаны на проверку этого метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -16,10 +16,4 @@ public class Point {
     return distance;
   }
 
-  public double distanceBetweenMethod(Point p1, Point p2) {
-    double distance;
-    //формула рассчёта расстояния между точками d=sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))
-    distance = Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)));
-    return distance;
-  }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/distanceToPointMethod.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/distanceToPointMethod.java
@@ -3,41 +3,47 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestDistanceBetweenMethod {
+public class distanceToPointMethod {
   @Test
   public void TestArea () {
     Point p1 = new Point (5,9);
     Point p2 = new Point (2,5);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),5.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),5.0);
+  }
+  @Test
+  public void TestArea1 () {
+    Point p1 = new Point (5,9);
+    Point p2 = new Point (2,5);
+    Assert.assertEquals (p2.distanceToPointMethod(p1),5.0);
   }
   @Test
   public void TestArea2 () {
     Point p1 = new Point (1000005,1000009);
     Point p2 = new Point (1000002,1000005);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),5.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),5.0);
   }
   @Test
   public void TestArea3 () {
     Point p1 = new Point (0,0);
     Point p2 = new Point (3,4);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),5.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),5.0);
   }
   @Test
   public void TestArea4 () {
     Point p1 = new Point (3,4);
     Point p2 = new Point (0,0);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),5.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),5.0);
   }
   @Test
   public void TestArea6 () {
     Point p1 = new Point (0,0);
     Point p2 = new Point (0,0);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),0.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),0.0);
   }
   @Test
   public void TestArea7 () {
     Point p1 = new Point (-6,-10);
     Point p2 = new Point (-9,-14);
-    Assert.assertEquals (p1.distanceBetweenMethod(p1,p2),5.0);
+    Assert.assertEquals (p1.distanceToPointMethod(p2),5.0);
   }
 }
