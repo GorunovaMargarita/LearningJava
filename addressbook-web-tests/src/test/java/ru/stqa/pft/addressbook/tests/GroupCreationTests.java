9ad4233459b9fc6ea92e8aba298190dd414e2627
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-02-09 18:57:17 +0300

    Лекции 2.5-2.7 - выделили общие классы для тестов на группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test12", "test22", "test32"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
