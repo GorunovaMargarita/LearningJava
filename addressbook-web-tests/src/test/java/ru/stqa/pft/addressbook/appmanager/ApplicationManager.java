cdafd7dab6921d05436f34609a8a15dd5578ce7a
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-02-09 19:29:20 +0300

    выделение общего базового класса (тесты на группы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -25,28 +25,9 @@ public class ApplicationManager {
     sessionHelper.login("admin", "secret");
   }
 
-
   public void stop() {
     wd.findElement(By.linkText("Logout")).click();
     wd.quit();
-  }
-
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
   public GroupHelper getGroupHelper() {
