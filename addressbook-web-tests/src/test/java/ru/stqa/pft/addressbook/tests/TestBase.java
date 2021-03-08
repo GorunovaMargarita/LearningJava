244261427ce0234d8fc1fedcd1c8bac5df2c19a9
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-08 18:50:11 +0300

    начало добавления возможности передать тип браузера для запуска

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,11 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 //Static - самостоятельные глобальные переменные и методы, для их запуска не нужно создавать экземпляр класса
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  /*protected static final ApplicationManager app
+        = new ApplicationManager(BrowserType.FIREFOX);*/
+protected static final ApplicationManager app
+        = new ApplicationManager(System.getProperty("browser"),BrowserType.CHROME);
+
 //для того, чтобы браузер не открывался каждый раз используем @BeforeSuite, а не @BeforeMethod
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
