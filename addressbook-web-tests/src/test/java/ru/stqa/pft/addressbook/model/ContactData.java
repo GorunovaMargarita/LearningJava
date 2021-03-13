a6813c6a31a986940108587fc3dfdd10b591bdca
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-13 16:58:17 +0300

    Получение данных из БД о контактах с условием

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,59 +1,96 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
+@Entity
+@Table(name="addressbook")
 
 public class ContactData {
+  @Id
+  @Column(name="id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name="firstName")
   private String firstName;
   private String middleName;
   @Expose
+  @Column(name="lastName")
   private String lastName;
+  @Transient
   private String nickName;
+  @Transient
   private String title;
+  @Transient
   private String company;
   @Expose
+  @Column(name="address")
+  @Type(type = "text")
   private  String address;
   @Expose
+  @Column(name="home")
+  @Type(type = "text")
   private  String homePhone;
   @Expose
+  @Column(name="mobile")
+  @Type(type = "text")
   private  String mobilePhone;
   @Expose
+  @Column(name="work")
+  @Type(type = "text")
   private  String workPhone;
+  @Transient
   private  String faxPhone;
   @Expose
+  @Transient
   private  String email1;
   @Expose
+  @Transient
   private  String email2;
   @Expose
+  @Transient
   private  String email3;
+  @Transient
   private  String homePage;
+  @Transient
   private  String birthDay;
+  @Transient
   private  String birthMonth;
+  @Transient
   private  String birthYear;
+  @Transient
   private  String anniversaryDay;
+  @Transient
   private  String anniversaryMonth;
+  @Transient
   private  String anniversaryYear;
+  @Transient
   private  String contactGroup;
+  @Transient
   private  String additionalAddress;
+  @Transient
   private  String additionalHome;
+  @Transient
   private  String additionalNotes;
+  @Transient
   private String AllPhones;
+  @Transient
   private String AllEmails;
+  @Column(name="photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
-
-  private File photo;
 
 
   public String getFirstName() {
@@ -324,6 +361,12 @@ public class ContactData {
     return Objects.hash(id, firstName, lastName, address);
   }
 
-
-
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            '}';
+  }
 }
