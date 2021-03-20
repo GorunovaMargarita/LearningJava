c711ab44275c608fe104f92db08185ac6c3654ea
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-20 17:13:33 +0300

    Подключение библиотеки HttpClient. Создание теста на логин.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.HttpSessionId;
 
 import java.io.File;
 import java.io.FileReader;
@@ -40,5 +41,11 @@ public class ApplicationManager {
   public void stop() {
     wd.quit();
   }
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
 
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
