6320fd8333dd0e8c35812e962103c961d93289e0
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-20 16:11:57 +0300

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+//Static - самостоятельные глобальные переменные и методы, для их запуска не нужно создавать экземпляр класса
+  /*protected static final ApplicationManager app
+        = new ApplicationManager(BrowserType.FIREFOX);*/
+protected static final ApplicationManager app
+        =new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+
+//для того, чтобы браузер не открывался каждый раз используем @BeforeSuite, а не @BeforeMethod
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
