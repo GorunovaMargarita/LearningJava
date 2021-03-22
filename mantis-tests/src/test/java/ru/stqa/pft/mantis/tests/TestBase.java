15e06b63f8256af996841033a1e6934419780cd5
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-22 16:06:41 +0300

    Подмена конфигурационного файла для избавления от капчи.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+
 public class TestBase {
 
 //Static - самостоятельные глобальные переменные и методы, для их запуска не нужно создавать экземпляр класса
@@ -17,10 +19,12 @@ protected static final ApplicationManager app
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"),"config_inc.php","config_inc.php.back");
   }
 
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
+    app.ftp().restore("config_inc.php.back","config_inc.php");
     app.stop();
   }
 }
