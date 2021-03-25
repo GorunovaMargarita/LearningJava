80e5af4bb8efc5c9f9a161dc696a76f620b521c6
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-25 15:12:34 +0300

    Дописан тест на регистрацию пользователя

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,17 +1,21 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-//Static - самостоятельные глобальные переменные и методы, для их запуска не нужно создавать экземпляр класса
-  /*protected static final ApplicationManager app
-        = new ApplicationManager(BrowserType.FIREFOX);*/
 protected static final ApplicationManager app
         =new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
@@ -26,5 +30,14 @@ protected static final ApplicationManager app
   public void tearDown() throws Exception {
     app.ftp().restore("config_inc.php.back","config_inc.php");
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m, Object[] p){
+    logger.info("Stop test " + m.getName()+ " with parameters " + Arrays.asList(p));
   }
 }
