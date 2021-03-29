65deb89a31eed63031e8853a15a9abba77fa5b9a
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-03-29 11:05:37 +0300

    Продолжение разработки теста на смену пароля

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/UserHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/UserHelper.java
@@ -1,18 +1,36 @@
 package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.By;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
 import ru.stqa.pft.mantis.model.UserData;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class UserHelper extends HelperBase{
   public UserHelper(ApplicationManager app) {
     super(app);
   }
 
-  public void changeUserPasswd(UserData user) {
+  public void changeUserPasswd(UserData user,String password) throws IOException, MessagingException {
     app.goTo().manage();
     app.goTo().manageUsers();
-    type(By.name("search"), user.getEmail());
+    type(By.id("search"), user.getEmail());
     click(By.cssSelector("input[type='submit']"));
-    click(By.xpath("//a[@href='/mantisbt-2.25.0/manage_overview_page.php']"));
+    wd.findElement(By.linkText(user.getUsername())).click();
+    wd.findElement(By.xpath("//input[@value='Сбросить пароль']")).click();
+    List<MailMessage> mailMessages = app.james().waitForMail(user.getUsername(), user.getPassword(), 60000);
+    String confirmationLink = findConfirmationLink(mailMessages, user.getEmail());
+    app.registration().finish(confirmationLink, password);
+  }
+
+  public String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
   }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/changePasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/changePasswordTests.java
@@ -4,18 +4,23 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.UserData;
 import ru.stqa.pft.mantis.model.Users;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
 
 public class changePasswordTests extends TestBase{
   @Test
-  public void testChangePassword() throws IOException {
+  public void testChangePassword() throws IOException, MessagingException {
     Users users = app.db().users();
     UserData userForChangePass = users.iterator().next();
     app.goTo().login(app.getProperty("web.adminLogin"),app.getProperty("web.adminPassword"));
     app.goTo().homePage();
     //UserData user = new UserData().withEmail("testmail1@mail.ru").withPassword("password");
-    app.user().changeUserPasswd(userForChangePass);
-    System.out.println(users);
+    String newPasswd = "newpass";
+    app.james().createUser(userForChangePass.getUsername(),userForChangePass.getPassword());
+    app.user().changeUserPasswd(userForChangePass,newPasswd);
+    assertTrue(app.newSession().login(userForChangePass.getUsername(),newPasswd));
   }
 
 
