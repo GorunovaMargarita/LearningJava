71195e75cb199b9dc4adf25cf7a251528942b110
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-04-01 10:43:27 +0300

    Добавлены методы создания, получения Issue. Добавлена возможность игнорировать тест, пока Issue не решён.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.mantis.appmanager;
 
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -21,10 +21,44 @@ public class SoapHelper {
     this.app = app;
   }
   public Set<Project> getProjects() throws MalformedURLException, ServiceException, RemoteException {
-    MantisConnectPortType mc = new MantisConnectLocator()
-            .getMantisConnectPort(new URL("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php?wsdl"));
+    MantisConnectPortType mc = getMantisConnect();
     ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
     return Arrays.asList(projects).stream().map((p)->new Project().withId(p.getId().intValue())
             .withName(p.getName())).collect(Collectors.toSet());
+  }
+
+  private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+    MantisConnectPortType mc = new MantisConnectLocator()
+            .getMantisConnectPort(new URL("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php"));
+    return mc;
+  }
+
+  public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = getMantisConnect();
+    String[] categories = mc.mc_project_get_categories("administrator", "root",BigInteger.valueOf(issue.getProject().getId()));
+    IssueData issueData=new IssueData();
+    issueData.setSummary(issue.getSummary());
+    issueData.setDescription(issue.getDescription());
+    issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
+    issueData.setCategory(categories[0]);
+    BigInteger issueId = mc.mc_issue_add("administrator", "root",issueData);
+    IssueData createdIssueData=mc.mc_issue_get("administrator", "root",issueId);
+    return new Issue().withId(createdIssueData.getId().intValue())
+            .withSummary(createdIssueData.getSummary())
+            .withDescription(createdIssueData.getDescription())
+            .withProject(new Project().withId(createdIssueData.getProject().getId().intValue()))
+            .withName(createdIssueData.getProject().getName());
+  }
+
+  public Issue getIssue(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = getMantisConnect();
+    IssueData issueData = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+    Issue issue = new Issue().withId(issueData.getId().intValue())
+            .withSummary(issueData.getSummary())
+            .withDescription(issueData.getDescription())
+            .withProject(new Project().withId(issueData.getProject().getId().intValue()))
+            .withName(issueData.getProject().getName())
+            .withStatus(issueData.getStatus().getName());
+    return issue;
   }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -3,14 +3,19 @@ package ru.stqa.pft.mantis.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+import ru.stqa.pft.mantis.model.Issue;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.Arrays;
 
 public class TestBase {
@@ -39,5 +44,19 @@ protected static final ApplicationManager app
   @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m, Object[] p){
     logger.info("Stop test " + m.getName()+ " with parameters " + Arrays.asList(p));
+  }
+
+  public boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    Issue issue = app.soap().getIssue(issueId);
+    if(issue.getStatus().equals("resolved")){
+      return false;
+    } else {
+      return true;
+    }
+  }
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 }
