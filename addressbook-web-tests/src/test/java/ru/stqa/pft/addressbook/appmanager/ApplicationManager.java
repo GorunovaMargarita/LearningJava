0d5781cb2f3761c6e5c8ef348f8ca0b8001de004
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-04-05 15:48:28 +0300

    Добавлена генерация отчёта при помощи Allure

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -84,5 +81,8 @@ public class ApplicationManager {
   }
   public ContactHelper contact() {
     return contactHelper;
+  }
+  public byte[] takeScreenshot(){
+    return ((TakesScreenshot) wd).getScreenshotAs(OutputType.BYTES);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,10 +4,8 @@ import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
@@ -18,7 +16,7 @@ import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-
+@Listeners (MyTestListener.class)
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
@@ -30,8 +28,9 @@ protected static final ApplicationManager app
 
 //для того, чтобы браузер не открывался каждый раз используем @BeforeSuite, а не @BeforeMethod
   @BeforeSuite(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp(ITestContext context) throws Exception {
     app.init();
+    context.setAttribute("app",app);
   }
 
   @AfterSuite(alwaysRun = true)
